43a2baf5e36aac5636be83c5d959bf5ea9c8cdba
Ice <panyaphon4906@gmail.com>
2025-01-30 21:28:10 +0700

    complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,41 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int a = 5;
+    char b = 'A';
+    char& c = b;
+    int* x = &a;
 
-	
-	
-	return 0;
+    char* y = &b;
+
+    int** z = &x;
+
+    cout << a << " " << b << " " << c << " ";
+    cout << (void*)x << " " << (void*)y << " " << (void*)z << endl;
+
+    cout << (void*)&a << " " << (void*)&b << " " << (void*)&c << " ";
+    cout << (void*)&x << " " << (void*)&y << " " << (void*)&z << endl;
+
+    c = 'F';
+
+    cout << a << " " << b << " " << c << " ";
+    cout << (void*)x << " " << (void*)y << " " << (void*)z << endl;
+
+    *y = 'W';
+
+    cout << a << " " << b << " " << c << " ";
+    cout << (void*)x << " " << (void*)y << " " << (void*)z << endl;
+
+    *x = 6;
+
+    cout << a << " " << b << " " << c << " ";
+    cout << (void*)x << " " << (void*)y << " " << (void*)z << endl;
+
+    **z = 7;
+
+    cout << a << " " << b << " " << c << " ";
+    cout << (void*)x << " " << (void*)y << " " << (void*)z << endl;
+
+    return 0;
 }
